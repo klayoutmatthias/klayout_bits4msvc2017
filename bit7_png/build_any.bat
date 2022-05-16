ab6c2dbd9b70d63b3796e9a3df138ccc6667f98a
klayoutmatthias <matthias@klayout.org>
2022-05-16 12:49:57 +0200

    Fixed libpng build

--- bit7_png/build_any.bat
+++ bit7_png/build_any.bat
@@ -12,17 +12,8 @@ git clone %git_url% sources
 cd sources
 git checkout --detach tags/%tag%
 
-rem copy zlib for reference
-mkdir zlib_copy
-mkdir zlib_copy\lib
-mkdir zlib_copy\include
-mkdir zlib_copy\bin
-xcopy /y %instdir%\..\zlib\libraries\*.* zlib_copy\lib
-xcopy /y %instdir%\..\zlib\include\*.* zlib_copy\include
-xcopy /y %instdir%\..\zlib\bin\*.* zlib_copy\bin
-
 rem point to zlib so cmake can detect it
-set PATH=%CD%\zlib_copy\bin;%PATH%
+set PATH=%instdir%\..\zlib\bin;%PATH%
 
 cd expat
 rem v141 is for MSVC 2017 (v142 for MSVC 2019)
